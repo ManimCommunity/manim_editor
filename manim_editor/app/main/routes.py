dd768f4910912c89b2988fee39f20976936cf240
christopher-besch <christopher.besch@gmx.de>
2021-10-19 20:39:58 +0200

    feat: flask works

--- manim_editor/app/main/routes.py
+++ manim_editor/app/main/routes.py
@@ -6,4 +6,4 @@ from app.main import bp
 @bp.route("/")
 @bp.route("/index")
 def index():
-    return render_template("index.html", title="Title")
+    return render_template("index.html", title="Index")
--- manim_editor/config.py
+++ manim_editor/config.py
@@ -1,3 +1,2 @@
-class Config():
-    # as this is an entirely offline application debugging is not a security concern
-    DEBUG = True
+class Config:
+    pass
