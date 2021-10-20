59bb00a4232d13a057a999b6d937f87fc3d47a79
christopher-besch <christopher.besch@gmx.de>
2021-10-20 19:26:45 +0200

    fixed manedit command

--- manim_editor/__main__.py
+++ manim_editor/__main__.py
@@ -1,9 +1,12 @@
 from .app import create_app
+import click
 
 
-def main():
+@click.command()
+@click.option("--debug", help="Launch Flask in Debug mode.")
+def main(debug=False):
     app = create_app()
-    app.run()
+    app.run(debug=debug)
 
 
 if __name__ == "__main__":
