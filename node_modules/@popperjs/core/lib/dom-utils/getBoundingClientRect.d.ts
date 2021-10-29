722dc85d78905606f8997bcbf2737ef961be6278
christopher-besch <christopher.besch@gmx.de>
2021-10-29 20:33:22 +0200

    ?

--- node_modules/@popperjs/core/lib/dom-utils/getBoundingClientRect.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ClientRectObject, VirtualElement } from "../types";
-export default function getBoundingClientRect(element: Element | VirtualElement, // eslint-disable-next-line unused-imports/no-unused-vars
-includeScale?: boolean): ClientRectObject;
