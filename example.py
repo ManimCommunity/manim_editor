29240e9df1d867fd5ab818b03bf65d17cda9ac88
christopher-besch <christopher.besch@gmx.de>
2021-11-03 15:25:19 +0100

    moved logo code in example.py

--- example.py
+++ example.py
@@ -1,5 +1,83 @@
 from manim import *
+from manim.mobject.geometry import ArrowTriangleFilledTip
+
 from manim_editor import PresentationSectionType
+
+
+class ManimEditorSquareLogo(ZoomedScene):
+    def construct(self):
+        self.camera.background_color = "#ece6e2"
+        logo_green = "#87c2a5"
+        logo_blue = "#525893"
+        logo_red = "#e07a5f"
+        triangle = Triangle(color=logo_red, fill_opacity=1, z_index=10).shift(1.2*RIGHT)
+        square = Square(color=logo_blue, fill_opacity=1, z_index=20).shift(UP)
+        circle = Circle(color=logo_green, fill_opacity=1, z_index=30).shift(LEFT)
+        logo = VGroup(triangle, square, circle).scale(2.5)
+        logo.move_to(ORIGIN)
+        self.add(logo)
+
+        l1 = Rectangle(height=1.7, width=0.6, stroke_width=0, fill_opacity=1, z_index=11)
+        l2 = l1.copy()
+        pause = VGroup(l1, l2).arrange(RIGHT)
+        pause.move_to(triangle.get_center_of_mass())
+        self.add(pause)
+
+        play = Triangle(color=WHITE, fill_opacity=1, z_index=21).rotate(-90*DEGREES).move_to(square.get_center()).scale(1.5).shift(0.2*RIGHT)
+        self.add(play)
+
+        arc = Arc(arc_center=circle.get_center(), radius=1.3, start_angle=-PI/16-PI/2, angle=-1.5*PI, color=WHITE, stroke_width=35, z_index=31)
+        tip = ArrowTriangleFilledTip(z_index=31, color=WHITE, start_angle=-PI/16-PI/2).scale(3)
+        tip.next_to(arc.get_end(), DOWN, buff=-0.1)
+
+        loop = VGroup(arc, tip)
+        self.add(loop)
+        self.camera.frame.scale(0.8)  # needs ZoomedScene
+
+
+class ManimEditorFullLogo(ZoomedScene):
+    def construct(self):
+        self.camera.background_color = BLACK
+        logo_green = "#87c2a5"
+        logo_blue = "#525893"
+        logo_red = "#e07a5f"
+        triangle = Triangle(color=logo_red, fill_opacity=1, z_index=10).shift(1.2*RIGHT)
+        square = Square(color=logo_blue, fill_opacity=1, z_index=20).shift(UP)
+        circle = Circle(color=logo_green, fill_opacity=1, z_index=30).shift(LEFT)
+        logo = VGroup(triangle, square, circle).scale(2.5)
+        logo.move_to(ORIGIN)
+        self.add(logo)
+
+        l1 = Rectangle(height=1.7, width=0.6, stroke_width=0, fill_opacity=1, z_index=11)
+        l2 = l1.copy()
+        pause = VGroup(l1, l2).arrange(RIGHT)
+        pause.move_to(triangle.get_center_of_mass())
+        self.add(pause)
+
+        play = Triangle(color=WHITE, fill_opacity=1, z_index=21).rotate(-90*DEGREES).move_to(square.get_center()).scale(1.5).shift(0.2*RIGHT)
+        self.add(play)
+
+        arc = Arc(arc_center=circle.get_center(), radius=1.3, start_angle=-PI/16-PI/2, angle=-1.5*PI, color=WHITE, stroke_width=35, z_index=31)
+        tip = ArrowTriangleFilledTip(z_index=31, color=WHITE, start_angle=-PI/16-PI/2).scale(3)
+        tip.next_to(arc.get_end(), DOWN, buff=-0.1)
+
+        loop = VGroup(arc, tip)
+        self.add(loop)
+        logo_all = VGroup(logo, play, pause, loop).scale(0.5)
+        self.camera.frame.scale(1.8)  # needs ZoomedScene
+
+        logo_black = "#343434"
+        M = MathTex(r"\mathbb{M}").scale(7).set_color(logo_black)
+        anim = Tex(r"\textbf{anim}", tex_template=TexFontTemplates.gnu_freeserif_freesans).scale(7).set_color(logo_black).scale(0.75748)
+
+        E = MathTex(r"\mathbb{E}").scale(7).set_color(logo_black)
+        ditor = Tex(r"\textbf{ditor}", tex_template=TexFontTemplates.gnu_freeserif_freesans).scale(7).set_color(logo_black).scale(0.75748)
+        x = VGroup(M, anim).arrange(RIGHT, buff=0.1)
+        y = VGroup(E, ditor).arrange(RIGHT, buff=0.1)
+        VGroup(x, y, logo_all).arrange(RIGHT, buff=0.5)
+        anim.align_to(M, DOWN)
+        ditor.align_to(M, DOWN)
+        self.add(x, y)
 
 
 class Test(Scene):
