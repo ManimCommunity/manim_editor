871b4411b1ebc068d68694caeecd310b26af1443
christopher-besch <christopher.besch@gmx.de>
2021-11-19 12:51:48 +0100

    bumping version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = '2021, ManimEditorProject'
 author = 'ManimEditorProject'
 
 # The full version, including alpha/beta/rc tags
-release = "v0.3.2"
+release = "v0.3.3"
 
 
 # -- General configuration ---------------------------------------------------
